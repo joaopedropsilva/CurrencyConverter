0d3e03caf5da08819cf622d85a5c3cd488d20b87
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2022-10-21 21:29:20 -0300

    chore(js): committing changes to be rechecked

--- src/js/updateValues.js
+++ src/js/updateValues.js
@@ -1,7 +1,6 @@
-const currenciesDivElement = document.querySelector(
-  ".result-section__currencies"
-);
+const currencyDiv = document.querySelector(".result-section__currencies");
 const inputValue = document.querySelector(".input-section__input");
+const convertButton = document.querySelector(".input-section__button");
 
 let changeStatus = false;
 
@@ -12,9 +11,9 @@ const currencies = [
 ];
 
 const baseCurrency = "BRL";
-const currenciesName = currencies.map(({ name }) => name);
+const currencyNames = currencies.map(({ name }) => name);
 
-const apiURL = `http://economia.awesomeapi.com.br/json/last/${currenciesName.join(
+const apiURL = `http://economia.awesomeapi.com.br/json/last/${currencyNames.join(
   "-BRL,"
 )}`;
 
@@ -41,6 +40,11 @@ const getCalculationFactors = async (getResponse, apiURL, changeStatus) => {
   }
 };
 
+// TODO: check this
+const factors = async () => {
+  await getCalculationFactors(getResponse, apiURL, changeStatus);
+};
+
 const changeCurrenciesValues = (
   currenciesElements,
   currencySymbols,
@@ -56,32 +60,34 @@ const changeCurrenciesValues = (
   }
 };
 
-const renderCurrenciesElements = (currencies, currenciesDivElement) => {
+const renderCurrencyElements = (currencies, currencyDiv) => {
   currencies.forEach((currency) => {
     const currencyElement = `
       <div class="result-section__currency-box">
         <ion-icon class="result-section__currency-logo" name="cash-outline"></ion-icon>
         <h2 class="result-section__currency-name">${currency.name}</h2>
-        <h2 class="result-section__currency-name">${currency.symbol}</h2>
+        <h2 class="result-section__currency-holder">${currency.symbol}<span class="result-section__currency-value"></span></h2>
       </div>
     `;
 
-    currenciesDivElement.innerHTML += currencyElement;
+    currencyDiv.innerHTML += currencyElement;
   });
 };
 
+// FIXME: finish this logic
+const getCurrencyElements = () => {
+  return document.querySelectorAll(".result-section__currency-value");
+};
+
+const addValuesToCurrencyElements = (changeStatus) => {
+  const curencyElements = getCurrencyElements();
+};
+
+const handleConvertButtonClick = (event) => {};
+
 window.onload = () => {
-  renderCurrenciesElements(currencies, currenciesDivElement);
-
-  (async () => {
-    let factors = await getCalculationFactors(
-      getResponse,
-      apiURL,
-      changeStatus
-    );
-
-    console.log(factors);
-  })();
+  renderCurrencyElements(currencies, currencyDiv);
 };
 
 // Add event listeners
+convertButton.addEventListener("click", handleConvertButtonClick);
